a8c1312dbd235de7c96382c6b11e7cb4489e477c
Kim-sangjoon <sang1009@naver.com>
2023-03-07 13:46:28 +0900

    탭추가

--- src/components/page/MainLayoutPage.js
+++ src/components/page/MainLayoutPage.js
@@ -4,7 +4,9 @@ import {
   Box,
   Toolbar,
   Grid,
-  Paper
+  Paper,
+  Tabs,
+  Tab
 } from '@mui/material';
 import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
 
@@ -12,31 +14,44 @@ const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'left',
   color: theme.palette.text.secondary,
   
 }));
 
+const leftComponentBox = {border: '1px solid #ddd', boxShadow: 'none', height: '300px', overflowY: 'auto', display:'flex', justifyContent: 'center', alignItems: 'center'}
+const rightCodeBox = {border: '1px solid #ddd', boxShadow: 'none', height: '300px', backgroundColor: '#333', overflowY: 'auto'}
+
+
 const MainLayoutPage = (props) => {
+    const [value, setValue] = React.useState(0);
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
     return (
         <>
             <Box className='contentboxwrap'>
                 <Toolbar />
                 <Box>
                 <h1>레이아웃</h1>
-                <h2>2Depth</h2>
+                <Tabs className='contentTab' value={value} onChange={handleChange}>
+                    <Tab label="분할 샘플" />
+                    <Tab label="2분할" />
+                    <Tab label="3분할" />
+                    <Tab label="4분할" />
+                </Tabs>
                 <p className='contText'>레아아웃 컴포넌트</p>
                     <Box sx={{ flexGrow: 1 }}>
                         <Grid container spacing={2}>
-                            <LiveProvider code="<button>버튼</button>">
+                            <LiveProvider code="<div>Hello World!</div>">
                                 <Grid item xs={12} md={6}>
-                                    <Item sx={{border: '1px solid #ddd', boxShadow: 'none', height: '300px', overflowY: 'auto'}}>
+                                    <Item sx={leftComponentBox}>
                                         <LivePreview />
                                         <LiveError />
                                     </Item>
                                 </Grid>
                                 <Grid item xs={12} md={6}>
-                                    <Item sx={{border: '1px solid #ddd', boxShadow: 'none', height: '300px', backgroundColor: '#333', overflowY: 'auto'}}>
+                                    <Item sx={rightCodeBox}>
                                         <LiveEditor />
                                     </Item>
                                 </Grid>
