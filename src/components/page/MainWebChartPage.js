fb13db5f95b3a9db54f7371e7e2baee467af12fc
김상준-재택 <sang1009@naver.com>
2023-07-12 14:15:01 +0900

    그리드랑 차트랑 추가

--- src/components/page/MainWebChartPage.js
+++ src/components/page/MainWebChartPage.js
@@ -6,7 +6,9 @@ import {
   Grid,
   Paper
 } from '@mui/material';
-
+import { PieChart, Pie } from "recharts";
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import CopyButton from '../component/CopyButton';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -16,7 +18,116 @@ const Item = styled(Paper)(({ theme }) => ({
   color: theme.palette.text.secondary,
 }));
 
+const leftComponentBox = {border: '1px solid #ddd', boxShadow: 'none', height: '400px', overflowY: 'auto', display:'flex', justifyContent: 'start', alignItems: 'start', textAlign: 'center'}
+const rightCodeBox = {border: '1px solid #ddd', boxShadow: 'none', height: '400px', backgroundColor: 'rgb(29, 31, 33)', overflowY: 'auto', textAlign: 'left'}
+
+// const data01 = [
+//     { name: "Group A", value: 400 },
+//     { name: "Group B", value: 300 },
+//     { name: "Group C", value: 300 },
+//     { name: "Group D", value: 200 }
+// ];
+// const data02 = [
+//     { name: "A1", value: 100 },
+//     { name: "A2", value: 300 },
+//     { name: "B1", value: 100 },
+//     { name: "B2", value: 80 },
+//     { name: "B3", value: 40 },
+//     { name: "B4", value: 30 },
+//     { name: "B5", value: 50 },
+//     { name: "C1", value: 100 },
+//     { name: "C2", value: 200 },
+//     { name: "D1", value: 150 },
+//     { name: "D2", value: 50 }
+// ];
+const scope = { 
+    PieChart, 
+    Pie,
+    Box,
+    Toolbar,
+    Grid,
+    Paper,
+};
+const code01 = `<PieChart width={300} height={300}>
+<Pie
+    data={[
+        { name: "Group A", value: 400 },
+        { name: "Group B", value: 300 },
+        { name: "Group C", value: 300 },
+        { name: "Group D", value: 200 }
+    ]}
+    dataKey="value"
+    cx={150}
+    cy={150}
+    outerRadius={60}
+    fill="#8884d8"
+/>
+<Pie
+    data={[
+        { name: "A1", value: 100 },
+        { name: "A2", value: 300 },
+        { name: "B1", value: 100 },
+        { name: "B2", value: 80 },
+        { name: "B3", value: 40 },
+        { name: "B4", value: 30 },
+        { name: "B5", value: 50 },
+        { name: "C1", value: 100 },
+        { name: "C2", value: 200 },
+        { name: "D1", value: 150 },
+        { name: "D2", value: 50 }
+    ]}
+    dataKey="value"
+    cx={150}
+    cy={150}
+    innerRadius={70}
+    outerRadius={90}
+    fill="#82ca9d"
+    label
+/>
+</PieChart>`;
+
+// const MyPieChart = () => {
+//     return(
+//         <PieChart width={300} height={300}>
+//             <Pie
+//                 data={data01}
+//                 dataKey="value"
+//                 cx={150}
+//                 cy={150}
+//                 outerRadius={60}
+//                 fill="#8884d8"
+//             />
+//             <Pie
+//                 data={data02}
+//                 dataKey="value"
+//                 cx={150}
+//                 cy={150}
+//                 innerRadius={70}
+//                 outerRadius={90}
+//                 fill="#82ca9d"
+//                 label
+//             />
+//         </PieChart>
+// )}
+
 const MainWebChartPage = (props) => {
+    React.useEffect(()=> {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    },[])
+    const [toast, setToast] = React.useState(false);
+    const handleToast = () => {
+        setToast(!toast)
+    }
+
+    function handleScrollTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
     return (
         <>
             <Box className='contentboxwrap'>
@@ -25,21 +136,28 @@ const MainWebChartPage = (props) => {
                 <h1>웹차트</h1>
                 <h2>2Depth</h2>
                 <p className='contText'>차트 컴포넌트</p>
-                    <Box sx={{ flexGrow: 1 }}>
+                <Box sx={{ flexGrow: 1 }}>
                         <Grid container spacing={2}>
-                            <Grid item xs={12} md={6}>
-                                <Item sx={{border: '1px solid #ddd', boxShadow: 'none'}}>
-                                    component area
-                                </Item>
-                            </Grid>
-                            <Grid item xs={12} md={6}>
-                                <Item sx={{border: '1px solid #ddd', boxShadow: 'none'}}>
-                                <code>code area</code>
-                                </Item>
-                            </Grid>
+                            <LiveProvider code={code01} scope={scope}>
+                                <Grid item xs={12} md={6}>
+                                    <Item sx={leftComponentBox}>
+                                        <LivePreview />
+                                        <LiveError />
+                                        {/* <MyPieChart /> */}
+                                    </Item>
+                                </Grid>
+                                <Grid item xs={12} md={6} sx={{position: 'relative'}}>
+                                    <Item sx={rightCodeBox}>
+                                        <LiveEditor />
+                                    </Item>
+                                    <CopyButton onClick={handleToast} code={code01} />
+                                </Grid>
+                            </LiveProvider>
                         </Grid>
                     </Box>
+                    <Box className='bottom-last'></Box>
                 </Box>
+                <Box className='top-button' onClick={handleScrollTop}>TOP</Box>
             </Box>
         </>
     );
--- src/components/page/MainWebGridPage.js
+++ src/components/page/MainWebGridPage.js
@@ -7,27 +7,79 @@ import {
   Paper
 } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import { LiveProvider, LiveEditor, LiveError, LivePreview } from "react-live";
+import CopyButton from '../component/CopyButton';
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
   ...theme.typography.body2,
   padding: theme.spacing(1),
-  textAlign: 'center',
   color: theme.palette.text.secondary,
 }));
 
-const rows = [
-    { id: 1, col1: 'Hello', col2: 'World' },
-    { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
-    { id: 3, col1: 'MUI', col2: 'is Amazing' },
-  ];
+const leftComponentBox = {border: '1px solid #ddd', boxShadow: 'none', height: '400px', overflowY: 'auto', display:'flex', justifyContent: 'start', alignItems: 'start', textAlign: 'center'}
+const rightCodeBox = {border: '1px solid #ddd', boxShadow: 'none', height: '400px', backgroundColor: 'rgb(29, 31, 33)', overflowY: 'auto', textAlign: 'left'}
+// const rows = [
+//     { id: 1, col1: 'Hello', col2: 'World' },
+//     { id: 2, col1: 'DataGridPro', col2: 'is Awesome' },
+//     { id: 3, col1: 'MUI', col2: 'is Amazing' },
+//   ];
   
-const columns = [
-    { field: 'col1', headerName: 'Column 1', width: 150 },
-    { field: 'col2', headerName: 'Column 2', width: 150 },
-];
+// const columns = [
+//     { field: 'col1', headerName: 'Column 1', width: 150 },
+//     { field: 'col2', headerName: 'Column 2', width: 150 },
+// ];
+
+const scope = { 
+    Box,
+    Toolbar,
+    Grid,
+    Paper,
+    Item,
+    DataGrid
+};
+
+const code01 =`<Box>
+<DataGrid
+    rows={[
+        { id: 1, col1: 'Hello', col2: 'World' },
+        { id: 2, col1: 'DataGrid', col2: 'is Awesome' },
+        { id: 3, col1: 'Awesome UI', col2: 'is Amazing' },
+        { id: 4, col1: 'Kim Sangjoon', col2: 'is Good' },
+        { id: 5, col1: 'AAA', col2: 'is BBB' },
+        { id: 6, col1: 'BBB', col2: 'is CCC' },
+        { id: 7, col1: 'CCC', col2: 'is DDD' },
+        { id: 8, col1: 'DDD', col2: 'is EEE' },
+        { id: 9, col1: 'EEE', col2: 'is FFF' },
+        { id: 10, col1: 'FFF', col2: 'is GGG' },
+    ]}                 
+    columns={[
+        { field: 'col1', headerName: 'Column 1', width: 150 },
+        { field: 'col2', headerName: 'Column 2', width: 150 }
+    ]}
+/>
+</Box>`;
+
+
 
 const MainWebGridPage = (props) => {
+    React.useEffect(()=> {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    },[])
+    const [toast, setToast] = React.useState(false);
+    const handleToast = () => {
+        setToast(!toast)
+    }
+
+    function handleScrollTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
     return (
         <>
             <Box className='contentboxwrap'>
@@ -38,21 +90,26 @@ const MainWebGridPage = (props) => {
                 <p className='contText'>그리드 컴포넌트</p>
                     <Box sx={{ flexGrow: 1 }}>
                         <Grid container spacing={2}>
-                            <Grid item xs={12} md={6}>
-                                <Item sx={{border: '1px solid #ddd', boxShadow: 'none'}}>
-                                    <div style={{ height: 300, width: '100%' }}>
-                                        <DataGrid rows={rows} columns={columns} />
-                                    </div>
-                                </Item>
-                            </Grid>
-                            <Grid item xs={12} md={6}>
-                                <Item sx={{border: '1px solid #ddd', boxShadow: 'none'}}>
-                                <code>code area</code>
-                                </Item>
-                            </Grid>
+                            <LiveProvider code={code01} scope={scope}>
+                                <Grid item xs={12} md={6}>
+                                    <Item sx={leftComponentBox}>
+                                        {/* <DataGrid rows={rows} columns={columns} /> */}
+                                        <LivePreview />
+                                        <LiveError />
+                                    </Item>
+                                </Grid>
+                                <Grid item xs={12} md={6} sx={{position: 'relative'}}>
+                                    <Item sx={rightCodeBox}>
+                                        <LiveEditor />
+                                    </Item>
+                                    <CopyButton onClick={handleToast} code={code01} />
+                                </Grid>
+                            </LiveProvider>
                         </Grid>
                     </Box>
+                    <Box className='bottom-last'></Box>
                 </Box>
+                <Box className='top-button' onClick={handleScrollTop}>TOP</Box>
             </Box>
         </>
     );
