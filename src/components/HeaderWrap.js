05620fc519295f376e3fee4eb7d9c9c9b3aedbbc
Kim-sangjoon <sang1009@naver.com>
2024-04-19 16:05:19 +0900

    수정

--- src/components/HeaderWrap.js
+++ src/components/HeaderWrap.js
@@ -152,9 +152,11 @@ const HeaderWrap = () => {
                     </Link>
                 </Box>
                 <Box sx={{display:'flex', alignItems: 'center', gap: '8px', color: theme.palette.text.secondary}}>
-                    <a href="https://github.com/Kim-sangjoon/react-sample-app" target="_blank" rel="noopener noreferrer">
-                        <Avatar sx={{border: '1px solid #ddd'}} alt="Kim Sangjoon" src="https://avatars.githubusercontent.com/u/33441969?s=400&u=948d9cd75f6f06bd96c1e8cceffd3fa299147bdc&v=4" />
-                    </a>
+                    <Tooltip title='Go to github' arrow>
+                        <a href="https://github.com/Kim-sangjoon/awesomeuiux" target="_blank" rel="noopener noreferrer">
+                            <Avatar sx={{border: '1px solid #ddd'}} alt="Kim Sangjoon" src="https://avatars.githubusercontent.com/u/33441969?s=400&u=948d9cd75f6f06bd96c1e8cceffd3fa299147bdc&v=4" />
+                        </a>
+                    </Tooltip>
                     <Typography sx={{fontSize: '18px', '@media(max-width: 600px)': {display: 'none'}}} component='span'>Kim Sangjoon</Typography>
                     <Tooltip title='Contact information' arrow>
                         <IconButton sx={{border: '1px solid #ddd'}} onClick={handleOpen}>
