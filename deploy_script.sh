e15773b7c389c3c5ec4a4e1f5211eb85305e34b1
Kim Sang Joon <sang1009@naver.com>
2024-04-19 14:30:23 +0900

    제발되라

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # 종속성 설치
-npm install --legacy-peer-deps
+# npm install --legacy-peer-deps
 
 # 프로젝트 빌드
-npm run build
+# npm run build
+
+# 프로젝트 배포
+npm run deploy
 
 # 빌드된 'build' 디렉토리를 gh-pages 브랜치로 푸쉬하기
-npx gh-pages -d build
+# npx gh-pages -d build
